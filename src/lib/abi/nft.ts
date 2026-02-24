8be11707e0c511f35c81f66685cac6a0c8176aae
Yaya N <yaya.nurcahya@gmail.com>
2026-02-24 23:44:50 +0700

    Added nft abi

---
+++ src/lib/abi/nft.ts
@@ -0,0 +1,23 @@
+export const nftAbi = [
+  {
+    inputs: [],
+    name: "mint",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "totalSupply",
+    outputs: [{ type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ type: "address" }],
+    name: "balanceOf",
+    outputs: [{ type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+] as const; // 'as const' is crucial for TypeScript type inference
