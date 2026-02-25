07bd603ac15632d20fa213425215ccb2b5462a70
Yaya N <yaya.nurcahya@gmail.com>
2026-02-25 20:02:09 +0700

    Deleted contract config

--- src/config/contract.ts
+++
@@ -1,9 +0,0 @@
-import { MyNFTAbi } from "../lib/abi/MyNFT";
-
-export const CONTRACT_CONFIG = {
-  address: {
-    mainnet: "0xYourContractAddressHere",
-    sepolia: import.meta.env.VITE_CONTRACT_ADDRESS, // Use environment variable for testnet address
-  },
-  abi: MyNFTAbi,
-} as const;
